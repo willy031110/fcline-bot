c611ad2912bd70f532a277272150afef37e30848
willy031110 <112925275+willy031110@users.noreply.github.com>
2024-05-15 12:24:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,10 @@
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import*
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, LocationMessage
 import requests
-import random
-
-#======這裡是呼叫的檔案內容=====
-from message import *
-from new import *
-from Function import *
-#======這裡是呼叫的檔案內容=====
-
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import time
-#======python的函數庫==========
 
 app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi('ZXxMakoI5GNuejiC7Igzm1wvqw3vDxHGRlicvQPM1qizx9eqUJSouLzo1rbTZxo24IWBi0E3AP8lBSOj7SRVt0GkK5Duowbfjn/Zgn8YPHKYfxJC90NHFr8ihfry5YKOjFiNPkHv+XGPydkBv5F0UAdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('4226f38b9cd8bce4d0417d29d575f750')
+
+# 設置 LINE Bot 的存取權杖和 Google Maps API 的金鑰
+LINE_CHANNEL_ACCESS_TOKEN ='ZXxMakoI5GNuejiC7Igzm1wvqw3vDxHGRlicvQPM1qizx9eqUJSouLzo1rbTZxo24IWBi0E3AP8lBSOj7SRVt0GkK5Duowbfjn/Zgn8YPHKYfxJC90NHFr8ihfry5YKOjFiNPkHv+XGPydkBv5F0UAdB04t89/1O/w1cDnyilFU='
 GOOGLE_MAPS_API_KEY = 'AIzaSyD5sX433QilH8IVyjPiIpqqzJAy_dZrLvE'
 
 @app.route("/callback", methods=["POST"])
@@ -50,6 +25,7 @@ def handle_location_message(event):
     reply_carousel_message(event["replyToken"], nearby_restaurants)
 
 def get_nearby_restaurants(lat, lng):
+    # 使用 Google Maps API 查詢附近餐廳
     url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=1500&type=restaurant&key={GOOGLE_MAPS_API_KEY}"
     response = requests.get(url)
     data = response.json()
@@ -64,7 +40,7 @@ def get_nearby_restaurants(lat, lng):
 def reply_carousel_message(reply_token, restaurants):
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {'ZXxMakoI5GNuejiC7Igzm1wvqw3vDxHGRlicvQPM1qizx9eqUJSouLzo1rbTZxo24IWBi0E3AP8lBSOj7SRVt0GkK5Duowbfjn/Zgn8YPHKYfxJC90NHFr8ihfry5YKOjFiNPkHv+XGPydkBv5F0UAdB04t89/1O/w1cDnyilFU='}"
+        "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"  # 使用 LINE Bot 的存取權杖作為授權 token
     }
     payload = {
         "replyToken": reply_token,
@@ -99,7 +75,4 @@ def reply_carousel_message(reply_token, restaurants):
 
 if __name__ == "__main__":
     app.run(debug=True)
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+
