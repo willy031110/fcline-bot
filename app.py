75e61ab972124ad83792d6175376b6826a02a77b
willy031110 <112925275+willy031110@users.noreply.github.com>
2024-05-15 11:59:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,41 +48,6 @@ def callback():
     return 'OK'
 
 
-# 處理訊息
-@handler.add(MessageEvent, message=LocationMessage)
-def handle_location_message(event):
-    lat = event.message.latitude
-    lng = event.message.longitude
-    restaurant = search_nearby_restaurant(lat, lng)
-    if restaurant:
-        reply_text = f"{restaurant['image_url']}我找到一家附近的餐廳：{restaurant['name']}，地址：{restaurant['address']}"
-    else:
-        reply_text = "抱歉，附近沒有找到餐廳"
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
-def search_nearby_restaurant(lat, lng):
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=1000&type=restaurant&key={GOOGLE_MAPS_API_KEY}"
-    response = requests.get(url)
-    data = response.json()
-    if 'results' in data and len(data['results']) > 0:
-        restaurant = random.choice(data['results'])
-        name = restaurant.get('name', 'Unknown')
-        address = restaurant.get('vicinity', 'Unknown')
-        if restaurant.get("photos") is None:
-            image_url= None
-        else:
-            photo_referance= restaurant["photos"][0]["photo_referance"]
-            photo_width=restaurant["photos"][0]["width"]
-            image_url="https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth={}".format(GOOGLE_MAPS_API_KEY,photo_referance,photo_width)
-            
-        return {'name': name, 'address': address,'image_url':image_url}
-    else:
-        return None
-
-if __name__ == "__main__":
-    app.run(debug=True)
- 
- 
-    
 def handle_location_message(event):
     lat = event["message"]["latitude"]
     lng = event["message"]["longitude"]
@@ -103,7 +68,7 @@ def reply_message(reply_token, message):
     # 回覆訊息給使用者
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {line_bot_api}"
+        "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"
     }
     payload = {
         "replyToken": reply_token,
@@ -119,51 +84,6 @@ def reply_message(reply_token, message):
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-    
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    lat = event.message.latitude
-    lng = event.message.longitude
-    msg = event.message.text
-    if '最新合作廠商' in msg:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '隨機推薦餐廳'in msg:
-        ##message = radomrestaurant(GOOGLE_MAPS_API_KEY,lat, lng)
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '最新活動訊息' in msg:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in msg:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in msg:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
-    else:
-        message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message)
-
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
         
         
 import os
