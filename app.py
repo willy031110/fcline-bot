cf589ae25d62eddfb2cc1684913e7a43cd9288a9
willy031110 <112925275+willy031110@users.noreply.github.com>
2024-05-29 00:11:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-import random
-import requests
-from linebot import WebhookHandler, LineBotApi
-from linebot.models import *
-from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from flask import Flask, request, abort
-import os
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import*
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, LocationMessage
+import tempfile, os
+import datetime
+import requests
+import time
 
 app = Flask(__name__)
-
-LINE_CHANNEL_ACCESS_TOKEN = 'ZXxMakoI5GNuejiC7Igzm1wvqw3vDxHGRlicvQPM1qizx9eqUJSouLzo1rbTZxo24IWBi0E3AP8lBSOj7SRVt0GkK5Duowbfjn/Zgn8YPHKYfxJC90NHFr8ihfry5YKOjFiNPkHv+XGPydkBv5F0UAdB04t89/1O/w1cDnyilFU='
+# Channel Access Token
+line_bot_api = LineBotApi('ZXxMakoI5GNuejiC7Igzm1wvqw3vDxHGRlicvQPM1qizx9eqUJSouLzo1rbTZxo24IWBi0E3AP8lBSOj7SRVt0GkK5Duowbfjn/Zgn8YPHKYfxJC90NHFr8ihfry5YKOjFiNPkHv+XGPydkBv5F0UAdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('4226f38b9cd8bce4d0417d29d575f750')
 GOOGLE_MAPS_API_KEY = 'AIzaSyD5sX433QilH8IVyjPiIpqqzJAy_dZrLvE'
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler('4226f38b9cd8bce4d0417d29d575f750')
 
-# 使用者請求資訊
-user_requests = {}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -35,12 +41,12 @@ def get_nearby_restaurants(latitude, longitude):
     url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius=500&type=restaurant&key={GOOGLE_MAPS_API_KEY}'
     response = requests.get(url)
     data = response.json()
-    return data.get('results', [])[:10]  # 只返回前10個餐廳
+    return data.get('results', [])[:10]  # 使用切片操作限制返回的餐廳列表不超過10個
 
 def format_restaurant_info(restaurant):
     photo_url = restaurant.get('photos')[0]['photo_reference'] if restaurant.get('photos') else ''
     name = restaurant.get('name', '')
-    address = restaurant.get('vicinity', '')[:60]  # 確保地址不超過60個字符
+    address = restaurant.get('vicinity', '')[:60]  # 對地址進行切片，確保長度不超過60個字符
     phone_number = restaurant.get('formatted_phone_number', '')
     return {
         'photo_url': f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_url}&key={GOOGLE_MAPS_API_KEY}',
@@ -71,40 +77,19 @@ def create_carousel_template(restaurants):
 def handle_text_message(event):
     user_id = event.source.user_id
     message_text = event.message.text
-    if message_text in ['推薦附近餐廳', '隨機推薦附近餐廳']:
-        user_requests[user_id] = {
-            'reply_token': event.reply_token,
-            'random': message_text == '隨機推薦附近餐廳'
-        }
+    if message_text == '推薦附近餐廳':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='請分享您的位置'))
+    elif message_text == '隨機推薦附近餐廳':
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='請分享您的位置'))
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
     user_id = event.source.user_id
-    if user_id in user_requests:
-        request_info = user_requests[user_id]
-        reply_token = request_info['reply_token']
-        is_random = request_info['random']
-
-        latitude = event.message.latitude
-        longitude = event.message.longitude
-        nearby_restaurants = get_nearby_restaurants(latitude, longitude)
-
-        if is_random and nearby_restaurants:
-            nearby_restaurants = [random.choice(nearby_restaurants)]
-
-        if nearby_restaurants:
-            carousel_template = create_carousel_template(nearby_restaurants)
-            try:
-                line_bot_api.reply_message(reply_token, carousel_template)
-            except LineBotApiError as e:
-                print(f"LineBotApiError: {e.status_code}, {e.error.message}")
-        else:
-            line_bot_api.reply_message(reply_token, TextSendMessage(text='附近沒有找到餐廳'))
-
-        del user_requests[user_id]  # 移除已處理的請求
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='請重新發送您的請求'))
+    latitude = event.message.latitude
+    longitude = event.message.longitude
+    nearby_restaurants = get_nearby_restaurants(latitude, longitude)
+    carousel_template = create_carousel_template(nearby_restaurants)
+    line_bot_api.reply_message(event.reply_token, carousel_template)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
